9451052b6a89155c5331cb6e74e77bf62013b266
Mehmet Selim CAN <MehmetSelimCN@hotmail.com>
2023-03-16 01:12:57 +0300

    Screen manager added for camera placement

---
+++ Row Match/Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour {
+
+    private void Awake() {
+        PrepareCamera();
+    }
+
+    private void PrepareCamera() {
+		var cam = GetComponent<Camera>();
+        float XPosition = (Grid.Cols - 1) / 2f;
+        float YPosition = (Grid.Rows - 1) / 2f;
+        cam.transform.position = new Vector3(XPosition, YPosition, -10);
+	}
+}
+
+
+
